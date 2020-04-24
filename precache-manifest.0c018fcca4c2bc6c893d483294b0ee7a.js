88fafeac513afa3145a2745950a65814a9cea3df
Akshay <akshaybhandaryg1@gmail.com>
2020-04-25 00:52:13 +0530

    Updates

---
+++ precache-manifest.0c018fcca4c2bc6c893d483294b0ee7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf5e1921717026628049d22aec8c809",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "178206da3de8c34bfaf9",
+    "url": "/monster-rolodex/static/css/main.638c9c7a.chunk.css"
+  },
+  {
+    "revision": "fbf00cd84deb0aba6f78",
+    "url": "/monster-rolodex/static/js/2.0fc44f82.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.0fc44f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178206da3de8c34bfaf9",
+    "url": "/monster-rolodex/static/js/main.5a6d806d.chunk.js"
+  },
+  {
+    "revision": "8e1b2dbf404abc0d760f",
+    "url": "/monster-rolodex/static/js/runtime-main.7619e950.js"
+  }
+]);
